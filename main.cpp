8dab6a03a0f93f1c38b3a84b4a996b3c87f768a0
mrpersonxd <64973075+mrpersonxd@users.noreply.github.com>
2021-09-07 15:49:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,20 +6,37 @@
 #include <vector>
 #include "engine.hpp"
 using namespace std;
+
 int main(){
     cattoEngine::game_array test;
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(globals::screenWidth, globals::screenHeight, "game");
-    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+    //SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()) * 2);
+    SetTargetFPS(120);
     test.Init();
-    test.GetData("test.txt");
+    Texture2D playertext = LoadTexture("Image.png");
+    Texture2D background = LoadTexture("Image1.png");
+    vector<cattoEngine::attack*> current;
+    cattoEngine::attack e(
+        { 0 , 0 },
+        5 ,
+        { 100 , 100 },
+        true,
+        playertext,
+        false ,
+        20
+    );
+    current.push_back(&e);
     while (!WindowShouldClose()){
-    //    test.character.speed = 20;
-    //    test.character.diagspeed = 10;
+        test.character.speed = 40;
+        test.character.diagspeed = 20;
+        test.character.size = 0;
         BeginDrawing();
         ClearBackground(RAYWHITE);
         test.UpdateLogic();
-        test.RenderArr();
+        (*current[0]).update();
+        test.RenderArr(playertext , background , current);
         EndDrawing();
       }
+
 }
